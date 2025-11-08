0f268b9f1addf7c1244b270988e27f900859f90e
NhlanhlaMags <69971222+NhlanhlaMags@users.noreply.github.com>
2025-11-08 15:14:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ This helps communities and municipalities **take quick, data-driven action** to 
 """)
 
 class WaterFeatureEngineer(BaseEstimator, TransformerMixin):
-def __init__ (self):
+ def __init__ (self):
  pass
 def fit(self, X, y=None):
 return self
