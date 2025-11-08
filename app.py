d0bcfd47d97bdc41e6c61e78ffc4ab27962db234
NhlanhlaMags <69971222+NhlanhlaMags@users.noreply.github.com>
2025-11-08 15:11:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -76,8 +76,7 @@ def __init__ (self):
 def fit(self, X, y=None):
  return self
 def transform(self, X):
- st.warning(" Using fallback WaterFeatureEngineer - no transformations applied")
-return X.copy()
+ st.warning(" Using fallback WaterFeatureEngineer - no transformations applied")return X.copy()
 
 # ---------------------------
 # Prediction Mode
